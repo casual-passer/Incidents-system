e8824ba4e51d379ccfc702e2a02db11aba50bfb7
Casual Passer <unparalleled.method@gmail.com>
2013-11-18 17:13:07 +0400

    Added models to admin page.

--- src/tickets/admin.py
+++ src/tickets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Status, Area, Department, Incident, IncidentComment
+
+admin.site.register(Status)
+admin.site.register(Area)
+admin.site.register(Department)
+admin.site.register(Incident)
+admin.site.register(IncidentComment)
+
 # Register your models here.
